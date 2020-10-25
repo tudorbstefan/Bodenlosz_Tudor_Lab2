9fa328b1d4d047eaa1e8f524c2c5540b0d52a40e
bod <bodenlosztudor123@gmail.com>
2020-10-25 20:50:12 +0200

    Am realizat modificarile din lab3

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,7 +10,7 @@ namespace Bodenlosz_Tudor_Lab2.Models
         public int OrderID { get; set; }
         public int CustomerID { get; set; }
         public int BookID { get; set; }
-
+        public DateTime OrderDate { get; set; }
         public Customer Customer { get; set; }
         public Book Book { get; set; }
 
